5d7f27a7b9e992fef4241603d328f93d45f2ebb6
saurabh <saurabhg476@gmail.com>
2018-02-22 18:06:36 +0530

    refactored k-means clustering into scikit like style for easy acess of variables

--- algorithms/K_means_clustering.py
+++ algorithms/K_means_clustering.py
@@ -3,23 +3,33 @@ import random
 import matplotlib.pyplot as plt
 
 
-def model(X,k):
+def initialize(X,k):
 
-    # number of examples
     m = X.shape[0]
+    random_indexes = random.sample(range(m), k)
+    centroids = X[random_indexes]
+    return centroids
 
-    # number of features
-    n = X.shape[1]
+
+def dist(a,b):
+
+    return np.linalg.norm(a-b,ord=2)
+
+
+def model(X, k,iterations=100):
+
+    m = X.shape[0]
+    k = k
 
     if k > m:
         raise ValueError("k should be less than or equal to number of points in training set")
 
-    centroids = initialize(X, k)
+    centroids = initialize(X,k)
 
     # array holding indexes of centroid with minimum distance from the point
     c = np.zeros((m, 1))
 
-    for p in range(10):
+    for p in range(iterations):
         # cluster assignment step
         for i,example in enumerate(X):
             min_dist = np.inf
@@ -33,25 +43,33 @@ def model(X,k):
             centroids[i] = X[np.squeeze(c) == i,:].mean(axis = 0,keepdims=True)
 
 
-    plt.figure(2)
-    plt.title("After clustering:")
-    plt.scatter(X[:,0], X[:,1], alpha=0.5,c=np.squeeze(c))
-    plt.show()
+    return centroids,c
 
-    return centroids
+class KMeans:
+
+    def __init__(self):
+        self.k = 0
+        self.centroids = None
+        self.iterations = 0
+        self.belongs = None
+
+    def model(self, X, k, iterations=100):
+        self.k = k
+        self.iterations = iterations
+
+        centroids , c  = model(X,k)
+
+        self.centroids = centroids
+        self.belongs = c
 
 
 
-def dist(a,b):
-
-    return np.linalg.norm(a-b,ord=2)
 
 
-def initialize(X, k):
 
-    m = X.shape[0]
-    random_indexes = random.sample(range(m),k)
-    centroids = X[random_indexes]
-    return centroids
+
+
+
+
 
 
--- tests/test_K-means_clustering.py
+++ tests/test_K-means_clustering.py
@@ -1,20 +1,30 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.datasets.samples_generator import make_blobs
 from algorithms import K_means_clustering as k_means
-import sklearn
+
 
 X,y = make_blobs(n_samples=100,centers=3,n_features=2,random_state=0)
 print(X.shape)
 print(y.shape)
 
 plt.figure(1)
+plt.title("Original")
 plt.scatter(X[:,0], X[:,1],c=y, alpha=0.5)
 plt.title("original")
 
 
-centroids = k_means.model(X,3)
-print(sklearn.__version__)
+clf = k_means.KMeans()
+clf.model(X,3,10)
+print(clf.centroids)
+print(clf.belongs)
 
-# plt.figure(3)
-# plt.scatter(centroids[:,0],centroids[:,1],c='g',alpha=0.9,marker='+')
-# plt.title("centroids")
+
+plt.figure(2)
+plt.title("After applying clustering")
+plt.scatter(X[:,0],X[:,1],c=np.squeeze(clf.belongs),alpha=0.5)
+
+
+plt.figure(2)
+plt.scatter(clf.centroids[:,0],clf.centroids[:,1],alpha=0.5,marker='+')
+plt.show()
